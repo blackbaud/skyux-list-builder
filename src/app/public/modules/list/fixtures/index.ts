f6800c8154de734c70b0e489988b47a47228a6a1
Alex Kingman <alex.kingman@blackbaud.com>
2019-02-01 14:17:13 -0500

    changed exports to use *

--- src/app/public/modules/list/fixtures/index.ts
+++ src/app/public/modules/list/fixtures/index.ts
@@ -1,7 +1,7 @@
-export { ListDualTestComponent } from './list-dual.component.fixture';
-export { ListEmptyTestComponent } from './list-empty.component.fixture';
-export { ListFilteredTestComponent } from './list-filtered.component.fixture';
-export { ListFixturesModule } from './list-fixtures.module';
-export { ListSelectedTestComponent } from './list-selected.component.fixture';
-export { ListTestComponent } from './list.component.fixture';
-export { ListViewTestComponent } from './list-view-test.component.fixture';
+export * from './list-dual.component.fixture';
+export * from './list-empty.component.fixture';
+export * from './list-filtered.component.fixture';
+export * from './list-fixtures.module';
+export * from './list-selected.component.fixture';
+export * from './list.component.fixture';
+export * from './list-view-test.component.fixture';

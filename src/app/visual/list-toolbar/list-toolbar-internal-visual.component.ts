98abd73546677559d7639ab6c450263aaefefb63
Alex Kingman <alex.kingman@blackbaud.com>
2019-02-04 13:37:29 -0500

    fixed import issues

--- src/app/visual/list-toolbar/list-toolbar-internal-visual.component.ts
+++ src/app/visual/list-toolbar/list-toolbar-internal-visual.component.ts
@@ -7,12 +7,11 @@ import {
   ListStateDispatcher
 } from '../../public/modules/list/state';
 
+// Internal component only used to get at ListStateDispatcher.
 @Component({
   selector: 'list-toolbar-internal-visual',
   template: ''
 })
-
-// Internal component only used to get at ListStateDispatcher.
 export class ListToolbarVisualInternalComponent implements OnInit {
   constructor(
     private dispatcher: ListStateDispatcher

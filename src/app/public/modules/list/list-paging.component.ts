49a02bbc170d7f8f850db324416c713da8f6cc9a
John Lyons <John.Lyons@blackbaud.me>
2020-10-15 09:21:14 -0400

    Hide paging component from list docs

--- src/app/public/modules/list/list-paging.component.ts
+++ src/app/public/modules/list/list-paging.component.ts
@@ -6,7 +6,7 @@ import { ListState } from './state/list-state.state-node';
 import { ListStateDispatcher } from './state/list-state.rxstate';
 
 /**
- * Provides a SKY UX-themed pagination control to display list data across multiple pages.
+ * @internal
  */
 export abstract class ListPagingComponent {
   protected initialized: BehaviorSubject<boolean> = new BehaviorSubject(false);
